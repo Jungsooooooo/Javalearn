67c3754cb8d92b2d443cb8f995ef65339b85c9e2
jungsoo <tnrlaos@gmail.com>
2021-11-23 23:10:22 +0900

    지네릭스 연습 1

---
+++ src/javalearn/generics1.java
@@ -0,0 +1,23 @@
+package javalearn;
+
+import java.util.ArrayList;
+
+class Subject{}
+class English extends Subject{}
+class Math extends Subject{}
+class PE{}
+
+public class generics1 {
+
+	public static void main(String[] args) {
+		
+		ArrayList<Subject>	SubjectList = new ArrayList<Subject>();
+		ArrayList<English>	E = new ArrayList<English>();
+		//        이부분과						이부분이 일치해야 에러가 나지 않고 제대로 기능을 구현한다.
+		
+		SubjectList.add(new English());
+		SubjectList.add(new Math());   // English와 Math가 Subject의 자손이기 때문에 대입이 가능하다.
+		//SubjectList.add(new PE()); 	-> 에러
+	}
+
+}
