bc573fb7bdc67d6b557858584365cc89ff81dc20
jungsoo <tnrlaos@gmail.com>
2021-11-24 23:52:15 +0900

    자바

--- src/javalearn/generics1.java
+++ src/javalearn/generics1.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 class Subject{}
 class English extends Subject{}
-class Math extends Subject{}
+class Math2 extends Subject{}
 class PE{}
 
 public class generics1 {
@@ -16,7 +16,7 @@ public class generics1 {
 		//        이부분과						이부분이 일치해야 에러가 나지 않고 제대로 기능을 구현한다.
 		
 		SubjectList.add(new English());
-		SubjectList.add(new Math());   // English와 Math가 Subject의 자손이기 때문에 대입이 가능하다.
+		SubjectList.add(new Math2());   // English와 Math가 Subject의 자손이기 때문에 대입이 가능하다.
 		//SubjectList.add(new PE()); 	-> 에러
 	}
 
