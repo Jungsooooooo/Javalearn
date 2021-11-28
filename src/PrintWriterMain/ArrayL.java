2133e1bb5817fb4ba5d5cc81a0024403a0fcdc16
jungsoo <tnrlaos@gmail.com>
2021-11-28 22:43:26 +0900

    arrayList에 phonebook.txt 넣기

--- src/PrintWriterMain/ArrayL.java
+++ src/PrintWriterMain/ArrayL.java
@@ -11,12 +11,12 @@ public class ArrayL {
 	public static void main(String[] args) {
 		try {
 		BufferedReader br = new BufferedReader( new FileReader("C:\\javaexercise\\javalearn\\src\\PrintWriterMain/phonebook.txt"));
-		PrintWriter pw = new PrintWriter("C:\\javaexercise\\javalearn\\src\\PrintWriterMain/phonebook.txt");
+		
 		 
 		
 		String line = "";
 		ArrayList<String> arraylist = new ArrayList<>();
-		for(int i = 1;(line = br.readLine())!=null;i++){ // 데이터가 끝날 때 까지 라인 단위로 읽어라.
+		for(int i = 0;(line = br.readLine())!=null;i++){ // 데이터가 끝날 때 까지 라인 단위로 읽어라.
 		 if(line.indexOf("")!=-1) //문자열을 포함하고 있는 정보 모두를 의미한다.
 		//	 	System.out.println(line);
 		
@@ -24,11 +24,13 @@ public class ArrayL {
 		}
 		
 		arraylist.add(9,"10"+", " + "스티브리"+"10"+", "+"010123412310");
-		arraylist.remove(8); //배열 8번째 값 제거
-		arraylist.clear(); //초기화
+		//arraylist.remove(8); //배열 8번째 값 제거
+		//arraylist.clear(); //초기화
 		// 이미 arraylist 배열 0~8은 값이 차있으니, 9번째 값으로 하여 새롭게 더해준다.
 		System.out.println(arraylist); // Arraylist 객체에 값이 담겨졌다.
-		System.out.println(arraylist.get(0));// 객체값 하나하나 읽을 수 있게 해준다.
+		//System.out.println(arraylist.get(0));// 객체값 하나하나 읽을 수 있게 해준다.
+		
+		
 		
 		
 		
@@ -36,5 +38,6 @@ public class ArrayL {
 		e.printStackTrace();
 	}
 
+	}
 }
-}
+
