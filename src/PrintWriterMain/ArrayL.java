96e3f351cbde646a5e717337d146c600331fcb86
김정수 <tnrlaos@gmail.com>
2021-12-02 22:15:13 +0900

    PhoneDTO에 담긴 텍스트 파일을 arrayList에 넣고

--- src/PrintWriterMain/ArrayL.java
+++ src/PrintWriterMain/ArrayL.java
@@ -2,6 +2,7 @@ package PrintWriterMain;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
@@ -9,27 +10,46 @@ import java.util.ArrayList;
 public class ArrayL {
 
 	public static void main(String[] args) {
+		
+		//String filepath 만들기.
+
 		try {
-		BufferedReader br = new BufferedReader( new FileReader("C:\\javaexercise\\javalearn\\src\\PrintWriterMain/phonebook.txt"));
+			FileReader fr = new FileReader("C:\\javaexercise\\javalearn\\src\\PrintWriterMain/phonebook.txt");
+		BufferedReader br = new BufferedReader(fr);
 		
-		 
-		
-		String line = "";
-		ArrayList<String> arraylist = new ArrayList<>();
-		for(int i = 0;(line = br.readLine())!=null;i++){ // 데이터가 끝날 때 까지 라인 단위로 읽어라.
-		 if(line.indexOf("")!=-1) //문자열을 포함하고 있는 정보 모두를 의미한다.
-		//	 	System.out.println(line);
-		
-		 arraylist.add(line);
+		String line= "";
+		String[]pdtoarr = line.split(",");
+		PhoneDTO pdto = new PhoneDTO(pdtoarr[0], pdtoarr[1], pdtoarr[2]);
+		ArrayList<PhoneDTO>arrayList = new ArrayList<>(); // arrayList 하나 만들어 주고
+		for(int i=0;(line=br.readLine())!=null;i++) {	// txt파일에 있는 내용 line 별로 읽어오는 기능
+			if(line.indexOf("")!=-1) {					// 안에 내용이 ("")문자열이 있으면
+				
+				
+				arrayList.add(pdto);					// 더해준다.
+			} else {
+				System.out.println("아무것도 들어있지 않습니다.");
+			}
+				
 		}
 		
-		arraylist.add(9,"10"+", " + "스티브리"+"10"+", "+"010123412310");
-		//arraylist.remove(8); //배열 8번째 값 제거
-		//arraylist.clear(); //초기화
-		// 이미 arraylist 배열 0~8은 값이 차있으니, 9번째 값으로 하여 새롭게 더해준다.
-		System.out.println(arraylist); // Arraylist 객체에 값이 담겨졌다.
-		//System.out.println(arraylist.get(0));// 객체값 하나하나 읽을 수 있게 해준다.
+		arrayList.add( new PhoneDTO("10","스티브리"+"10","010123412310"));
 		
+		//System.out.println(arrayList.get(0).getName());
+		//arrayList.remove(8); //배열 8번째 값 제거
+		//arrayList.clear(); //초기화
+		// 이미 arraylist 배열 0~8은 값이 차있으니, 9번째 값으로 하여 새롭게 더해준다.
+		//System.out.println(arrayList); // Arraylist 객체에 값이 담겨졌다.
+		//System.out.println(arrayList.get(9));// 객체값 하나하나 읽을 수 있게 해준다.
+		
+		
+		//객체를 다시 스트링으로 바꿔 줘야 한다.
+		PrintWriter pw = new PrintWriter("C:\\javaexercise\\javalearn\\src\\PrintWriterMain/phonebook.txt");
+		for(int i = 0; i<arrayList.size(); i++) {
+			pw.println(arrayList.get(i));
+		}
+			
+			
+			pw.close();
 		
 		
 		
