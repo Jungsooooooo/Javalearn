cacaaa1c3e69af5a616d2879fdf8932cff22d256
jungsookim <tnrlaos@gmail.com>
2022-08-22 22:56:16 +0900

    백준 조건문 9498번

---
+++ src/javalearn/conditionalSentence1.java
@@ -0,0 +1,44 @@
+package javalearn;
+
+public class conditionalSentence1 {
+
+	public static void main(String[] args) {
+		// 시험 점수를 입력받아 90 ~ 100점은 A, 
+//		80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 
+//		나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+		
+		int score = 77;
+		
+		switch(score/10) {
+			case  10  : System.out.println("A");
+				break;
+		
+			case 9 : System.out.println("A");
+				break; 
+			 
+			case 8 : System.out.println("B");
+				break;
+				
+			case 7 : System.out.println("C");
+				break;
+			case 6 : System.out.println("D");
+				break;
+			default : System.out.println("F");	
+				
+		}
+		
+		if(score>=90) {
+			System.out.println("A");
+		}else if(score>=80) {
+			System.out.println("B");
+		}else if (score>=70) {
+			System.out.println("C");
+		}else if(score>=60) {
+			System.out.println("D");
+		}else {
+			System.out.println("F");
+		}
+
+	}
+
+}
